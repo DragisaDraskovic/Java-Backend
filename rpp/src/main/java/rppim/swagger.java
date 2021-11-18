4d0cbb4fc7280d85f8d6f806bd99cf309e8533c7
DragisaDraskovic <44862909+DragisaDraskovic@users.noreply.github.com>
2021-11-18 15:32:16 +0100

    Update swagger.java

--- rpp/src/main/java/rppim/swagger.java
+++ rpp/src/main/java/rppim/swagger.java
@@ -9,12 +9,12 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@EnableSwagger2				//ukljucuje swagger
-@Configuration				//govori da je klasa koja se konfigurise
+@EnableSwagger2			
+@Configuration
 public class swagger {
 
 		@Bean
-		public Docket api()	 {										//ova klasa nam omogucava da opisemo sve metode koje imamo u nasim rest kontrolerima
+		public Docket api()	 {									
 			return new Docket(DocumentationType.SWAGGER_2)
 					.select()
 					.apis(RequestHandlerSelectors.any() )
