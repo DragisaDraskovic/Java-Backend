5ce166022b1e619fe3af1647ed885f46b9452992
DragisaDraskovic <44862909+DragisaDraskovic@users.noreply.github.com>
2021-11-18 15:36:50 +0100

    Update NacionalnostRestController.java

--- rpp/src/main/java/rppim/ctrl/NacionalnostRestController.java
+++ rpp/src/main/java/rppim/ctrl/NacionalnostRestController.java
@@ -24,17 +24,14 @@ import rppim.reps.NacionalnostRepository;
 @RestController
 public class NacionalnostRestController {
 
-	@Autowired										//anotacija koja automacki povezuje u ovom slucaju sa nacionalnostRepository
+	@Autowired										
 	NacionalnostRepository nacionalnostRepository;
 	
-	/*
-	 * JdbcTemplate klasa pojednostavljuje koriscenje JDBC (Java Database Connectivity), objekat ove klase ce se koristiti u metodi delete() i zato je 
-	 * deklarisana i anotirana sa @Autowired
-	 */
+	
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
 	
-	@CrossOrigin			//daje dozvolu da se spoje iz stringa i vsc-a
+	@CrossOrigin			
 	@ApiOperation(value = "Returns collection of all Nacionalnost from database.")
 	@GetMapping("nacionalnost")
 	public Collection<Nacionalnost> getAll(){
@@ -55,12 +52,6 @@ public class NacionalnostRestController {
 		return nacionalnostRepository.findByNazivContainingIgnoreCase(naziv);
 	}
 	
-	/*
-	 * HTTP Post je jedna od HTTP metoda koja je analogna opciji CREATE iz CRUD operacija.
-	 * ANOTACIJA @PostMapping se koristi kako bi se mapirao HTTP POST zahtev. Predstavlja skracenu verziju metode @RequestMapping(method = RequestMethod.POST) u
-	 * konkretnom slucaju HTTP POST zahtevi upuceni na adresu localhost:8083/nacionalnost bice prosledjeni ovoj metodi. Poziv metode 
-	 * nacionalnostRepository.save(nacionalnost) ce sacuvati prosledjenu nacionalnost u bazu podataka
-	 */
 	
 	@CrossOrigin
 	@ApiOperation(value = "Add instance of Nacionalnost to database.")
@@ -70,34 +61,23 @@ public class NacionalnostRestController {
 		return new ResponseEntity<HttpStatus>(HttpStatus.CREATED);
 	}
 	
-	/*
-	 * HTTP PUT je jedna od HTTP metoda koja je analogna opciji UPDATE iz CRUD opredacija. 
-	 * Anotacija @PutMapping se koristi kako bi se mapirao HTTP PUT zahtev. Pedstavlja skracenu verziju metode @RequestMapping(method = RequestMethod.PUT)
-	 * U konkretnom slucaju HTTP PUT zahtevi upuceni na adresu localhost:8083/nacionalnost/{id} bice prosledjeni ovoj metodi.
-	 * Poziv metode nacionalnostRepository.save(nacionalnost) ce imati nacionalnost sa prosledjenim ID-em i prosledjenim sadrzajem u bazi podataka
-	 */
-	
+
 	
 	@CrossOrigin
 	@ApiOperation(value = "Updates Nacionalnost that has ID that was forwarded as path variable with values forwarded in Request Body")
-	@PutMapping(value = "nacionalnost/{id}", produces = "application/json; charset-utf-a")		//put je za update, on govori idi na stranicu nacionalnost id
+	@PutMapping(value = "nacionalnost/{id}", produces = "application/json; charset-utf-a")		
 	public ResponseEntity<HttpStatus> updateNacionalnost(@RequestBody Nacionalnost nacionalnost,
 			@PathVariable("id") Integer id) {
-		if(nacionalnostRepository.existsById(id)) {		//ova metoda vraca true ako postoji nacionalnost sa prosledjenim id-em i onda izvrsava ovo ispod
-			nacionalnost.setId(id); 		//prosledjenu nacionalnost u request bodi i id-em u path variable izmeniti
-			nacionalnostRepository.save(nacionalnost);		//sacuvati nacionalnost
-			return new ResponseEntity<HttpStatus>(HttpStatus.OK);	//vratiti http u ovom slucaju ok
+		if(nacionalnostRepository.existsById(id)) {		
+			nacionalnost.setId(id); 		
+			nacionalnostRepository.save(nacionalnost);		
+			return new ResponseEntity<HttpStatus>(HttpStatus.OK);	
 		}
 		
-		return new ResponseEntity<HttpStatus>(HttpStatus.NOT_FOUND);		//vratiti http u ovom slucaju not found
+		return new ResponseEntity<HttpStatus>(HttpStatus.NOT_FOUND);		
 	}
 	
-	/*
-	 * HTTP DELETE je jedna od HTTP metoda koja je analogna opciji DELETE iz CRUD opredacija
-	 * Anotacija @DeleteMapping se koristi kako bi se mapirao HTTP DELETE zahtev.Pedstavlja skracenu verziju metode 
-	 * @RequestMapping(method = RequestMethod.DELETE). U konkretnom slucaju HTTP PUT zahtevi upuceni na adresu localhost:8083/nacionalnost/{id} bice prosledjeni
-	 * ovoj metodi. Pozivi metode nacionalnostRepository.deleteByID(id) ce obrisati nacionalnost sa prosledjenim ID-em iz baze podataka	
-	 */
+
 	
 	@CrossOrigin
 	@ApiOperation(value = "Deletes Nacionalnost with ID that was forwarded as path variable")
